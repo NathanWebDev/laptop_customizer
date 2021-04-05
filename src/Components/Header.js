3c5eb0d13d43de3a86044afc9fba4c85d32dd817
NathanWebDev <pikminnicus@gmail.com>
2021-04-04 22:14:08 -0500

    Initial Upload
    Initial Upload of Files

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render(){
+        return(
+        <h1>
+            <header>
+            <h1>ELF Computing | Laptops</h1>
+            </header>
+        </h1>
+        )
+    }
+}
